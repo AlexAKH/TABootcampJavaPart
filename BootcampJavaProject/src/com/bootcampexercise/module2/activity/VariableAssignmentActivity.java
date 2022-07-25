e955a4bdf2fd781ab8019609912b7ea55b60f406
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-25 15:29:54 +0300

    variablesactivities

---
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexercise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+        int i = 5;
+        i = 10;
+        System.out.println(i)
+
+    }
