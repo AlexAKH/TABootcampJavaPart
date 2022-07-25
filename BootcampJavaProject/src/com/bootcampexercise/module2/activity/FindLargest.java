695ea4098123ff00d8704586caf2ea7015fec785
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-26 00:14:34 +0300

    FindLargest

---
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/FindLargest.java
@@ -0,0 +1,16 @@
+package com.bootcampexercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String args[]) {
+        int x = 15;
+        int y = 12;
+
+        if (x > y) {
+            System.out.println("The value of x is greater than y");
+        } else {
+            System.out.println("The value of x is less than y");
+        }
+
+
+    }
+}
