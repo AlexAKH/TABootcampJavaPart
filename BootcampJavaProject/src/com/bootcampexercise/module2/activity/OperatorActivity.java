5b5a812c41187c858b285d3b9c8665a298752371
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-25 16:40:22 +0300

    OperatorActivity

---
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+    int i=6;
+    int x=7;
+    int sum = x + i;
+    System.out.println ("The sum of numbers is:" +sum);
+}
+}
