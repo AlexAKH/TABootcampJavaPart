c6320090e2c7abeeba397dc47b95fed41ce172ea
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-25 18:06:22 +0300

    NumToWords final correction

--- BootcampJavaProject/src/com/bootcampexercise/module2/activity/NumToWords.java
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/NumToWords.java
@@ -3,49 +3,18 @@ package com.bootcampexercise.module2.activity;
 public class NumToWords {
     public static void main(String[] args) {
      int x = 5;
-        switch(x) {
-            case 1:
-                System.out.println("The number is "+x+" = One");
-                break;
-
-            case 2:
-                System.out.println("The number is "+x+" = Two");
-                break;
-
-            case 3:
-                System.out.println("The number is "+x+" = Three");
-                break;
-
-            case 4:
-                System.out.println("The number is "+x+" = Four");
-                break;
-
-            case 5:
-                System.out.println("The number is "+x+" = Five");
-                break;
-
-            case 6:
-                System.out.println("The number is "+x+" = Six");
-                break;
-
-            case 7:
-                System.out.println("The number is "+x+" = Seven");
-                break;
-
-            case 8:
-                System.out.println("The number is "+x+" = Eight");
-                break;
-
-            case 9:
-                System.out.println("The number is "+x+" = Nine");
-                break;
-
-            case 10:
-                System.out.println("The number is "+x+" = Ten");
-                break;
-
-            default:
-                System.out.println("The number is out of range");
+        switch (x) {
+            case 1 -> System.out.println("The number is " + x + " = One");
+            case 2 -> System.out.println("The number is " + x + " = Two");
+            case 3 -> System.out.println("The number is " + x + " = Three");
+            case 4 -> System.out.println("The number is " + x + " = Four");
+            case 5 -> System.out.println("The number is " + x + " = Five");
+            case 6 -> System.out.println("The number is " + x + " = Six");
+            case 7 -> System.out.println("The number is " + x + " = Seven");
+            case 8 -> System.out.println("The number is " + x + " = Eight");
+            case 9 -> System.out.println("The number is " + x + " = Nine");
+            case 10 -> System.out.println("The number is " + x + " = Ten");
+            default -> System.out.println("Given value is not in the range 1-10");
         }
     }
 }
