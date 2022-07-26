2430f84efd236a017e3161695542ba727e97d828
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-26 10:03:32 +0300

    PrintNumWithWhile

---
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,14 @@
+package com.bootcampexercise.module2.activity;
+
+public class PrintNumWithWhile {
+        public static void main(String[] args) {
+
+        // Print all even numbers less than 100
+        int i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 1;
+        }
+            }
+
+}
