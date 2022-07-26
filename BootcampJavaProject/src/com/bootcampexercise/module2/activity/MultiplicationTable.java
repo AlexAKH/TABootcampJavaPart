de2d12cdf74bb8f0052c93b68363563460070bac
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-26 12:12:53 +0300

    NumToWordsWithMethodCalling_corrected1

--- BootcampJavaProject/src/com/bootcampexercise/module2/activity/MultiplicationTable.java
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/MultiplicationTable.java
@@ -3,7 +3,7 @@ package com.bootcampexercise.module2.activity;
 public class MultiplicationTable {public static void main(String[] args) {
     for (int i = 11; i <= 20; i++) {
         System.out.println("*** Table of "+i + " ***");
-        for (int j = 1; j <= 20; j++) {
+        for (int j = 1; j <= 10; j++) {
             System.out.println(i + "X" + j +" = " + i*j);
         }
     }
