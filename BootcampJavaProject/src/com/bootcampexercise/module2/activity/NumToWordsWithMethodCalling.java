d35fd735df3fc8e49a7319e92f036bf73e7fd0e8
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-26 12:03:46 +0300

    NumToWordsWithMethodCalling

---
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/NumToWordsWithMethodCalling.java
@@ -0,0 +1,13 @@
+package com.bootcampexercise.module2.activity;
+
+public class NumToWordsWithMethodCalling {
+    public static void main(String[] args) {
+        // Call method here
+    }
+
+    public static void numToWordsLogic(int x) {
+        //logic of numToWords Class
+    }
+
+}
+
