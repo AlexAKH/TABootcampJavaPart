7a618ba5e5ca8d9f5724437020c5374833f3c901
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-26 13:53:21 +0300

    NumToWordsWithMethodCalling corrected

--- BootcampJavaProject/src/com/bootcampexercise/module2/activity/NumToWordsWithMethodCalling.java
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/NumToWordsWithMethodCalling.java
@@ -2,12 +2,25 @@ package com.bootcampexercise.module2.activity;
 
 public class NumToWordsWithMethodCalling {
     public static void main(String[] args) {
-        // Call method here
-    }
+        NumToWords numToWords = new NumToWords();
+        numToWordsLogic(5);
 
+    }
     public static void numToWordsLogic(int x) {
-        //logic of numToWords Class
+            switch (x) {
+            case 1 -> System.out.println("The number is " + x + " = One");
+            case 2 -> System.out.println("The number is " + x + " = Two");
+            case 3 -> System.out.println("The number is " + x + " = Three");
+            case 4 -> System.out.println("The number is " + x + " = Four");
+            case 5 -> System.out.println("The number is " + x + " = Five");
+            case 6 -> System.out.println("The number is " + x + " = Six");
+            case 7 -> System.out.println("The number is " + x + " = Seven");
+            case 8 -> System.out.println("The number is " + x + " = Eight");
+            case 9 -> System.out.println("The number is " + x + " = Nine");
+            case 10 -> System.out.println("The number is " + x + " = Ten");
+            default -> System.out.println("Given value is not in the range 1-10");
+        }
     }
-
 }
+
 
