7b1dffaf078d1979ff4b970ce40ec33273e9db9e
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-07-26 17:19:37 +0300

    PrintNumWithWhile corrected

--- BootcampJavaProject/src/com/bootcampexercise/module2/activity/PrintNumWithWhile.java
+++ BootcampJavaProject/src/com/bootcampexercise/module2/activity/PrintNumWithWhile.java
@@ -1,14 +1,16 @@
 package com.bootcampexercise.module2.activity;
 
 public class PrintNumWithWhile {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        // Print all even numbers less than 100
-        int i = 0;
-        while (i < 100) {
+        // Print all odd numbers less than 100
+        int i = 1;
+            System.out.println("The Odd numbers are: " );
+            while (i <= 100) {
+            if(i%2 != 0)
             System.out.println(i);
-            i = i + 1;
-        }
+            i++;
             }
+        }
 
-}
+    }
